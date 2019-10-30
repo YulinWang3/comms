27b08b5a99ce54fb4c6e16586a79e1ff52a0b33a
Rafe <HarmonicRealm@github/.com>
2019-10-30 09:46:03 -0400

    first draft of dataStore

---
+++ dataStore.py
@@ -0,0 +1,20 @@
+import socket, sys, time
+
+def receive_from_arduino_pinger(s, port):
+        buf, address = s.recvfrom(port)
+        # do stuff
+        # return stuff
+        return buf
+
+def send_to_arduino_pinger(s, port, collected_values):
+        s.sendto(collected_values.encode('utf-8'), ('10.0.0.1', 200))
+
+if __name__ == "__main__":
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        listening_on = ('10.0.0.1', 300)
+        s.bind(listening_on)
+
+        while True:
+                collected_values = receive_from_arduino_pinger(s, 100)
+                send_to_arduino_pinger(s, 200, collected_values)
+
