4889da2294cca4a192c2231831a8f6520c6da5d5
Rafe <HarmonicRealm@github/.com>
2019-10-30 09:35:38 -0400

    fundementally flawed on the first push, fixed

--- arduinoPinger.py
+++ arduinoPinger.py
@@ -1,16 +1,17 @@
 # SYSC 3010 F 2019
 
 # Arduino Pinger
-# - sends on 10.0.0.1:100
-# - receives on 10.0.0.1:200
+# - sends to 10.0.0.1:100 (data collector)
+# - receives on 10.0.0.1:200 (self)
+# - sends to 10.0.0.1:300 (data store)
 
 # Data Collector
-# - sends on 10.0.0.1:300
-# - receives on 10.0.0.1:400
+# - receives on 10.0.0.1:100 (self)
+# - sends to 10.0.0.1:200 (arduino pinger)
 
 # Data Store
-# - sends on 10.0.0.1:500
-# - receives on 10.0.0.1:600
+# - receives on 10.0.0.1:300 (self)
+# - sends to 10.0.0.1:200 (arduino pinger)
 
 import socket, sys, time
 
@@ -19,29 +20,24 @@ def ping_arduino(s, port):
 	data = 'test'
 	s.sendto(data.encode('utf-8'), server_address)
 
-def receive_arduino(s, port):
-	server_address = ('10.0.0.1', port)
-	s.bind(server_address)
-	buf, address = s.recvfrom(port)
-	return buf
-
 def ping_data_store(s, port, collected_values):
 	server_address = ('10.0.0.1', port)
 	s.sendto(collected_values.encode('utf-8'), server_address)
 
-def receive_data_store(s, port):
-	print('nice')
 
 if __name__ == "__main__":
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	listening_on = ('10.0.0.1', 200)
+	s.bind(listening on)
 
 	while True:
 		print("PINGING ARDUINO")
 		ping_arduino(s, 100)
-		collected_values = receive_arduino(s, 200)
+		collected_values = s.recvfrom(100)
 		print("RECEIVED FROM ARDUINO, PINGING DATA_STORE")
 		ping_data_store(s, 300, collected_values)
-		receive_data_store()
+		ack_store = s.recvfrom(300)
+		print(ack_store)
 		print("RECEIVED FROM DATA_STORE")
 		time.sleep(10)
 
