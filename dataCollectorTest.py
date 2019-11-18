750b0f05a9ce5b4a2a02874ba2cf6ad1bebc3ed7
AndrewCMoore <andrewmoore9815@gmail.com>
2019-11-17 23:05:38 -0500

    Created a text file for automated testing, added comments to test files

--- dataCollectorTest.py
+++ dataCollectorTest.py
@@ -1,39 +1,16 @@
+import dataCollectortesting, json, unittest
 
-'''
-Tests to be run:
-
-1. tempAcceptedRangeHigh
-2. tempAcceptedRangeLow
-3. turbidityAcceptedRangedHigh
-4. turbidityAcceptedRangeLow
-5. pHAcceptedRangeHigh
-6. pHAcceptedRangeLow
-7. turbidityTempRangeHigh
-8. turbidityTempRangeLow
-9. pHTempRangeHigh
-10. phTempRangeLow
-11. locationError
-12. depthAcceptedRange
-
-'''
-
-
-'''
-Test temperature out of accepted range (-80 -> 150C)
-Test# temperature out of probe range (-55C -> 125 C)
-Test #6 Boolean JSON ph fail
-Test# pH out of accepted range (0 -> 14)
-Test ph sensor temperature out of probe range (-10 C -> 50 C)
-Test #7 Boolean JSON turbidity
-Test turbidity probe temperature out of range (5C -> 90C)
-Test Turbidity probe for accepted values (0-3000 NTU)
-'''
-
-import dataCollectortesting, json
-
-f = open("ErrorTesting.txt", "r")
-for x in f:
-    dataCollectortesting.test_values = f.readline()
-    print(dataCollectortesting.test_values)
-    dataCollectortesting.collected_values = json.loads(dataCollectortesting.test_values)
-    dataCollectortesting.main()
+# Open a premade text file to read from to automatically run tests
+filepath = 'ErrorTesting.txt'
+with open(filepath) as fp:
+    # Read the first line
+    line = fp.readline()
+    while line:
+        # Change the input value for different error cases
+        dataCollectortesting.collected_values = json.loads(line)
+        # Load tests from dataCollectesting.py
+        suite = unittest.defaultTestLoader.loadTestsFromModule(dataCollectortesting)
+        # Run tests
+        test = unittest.TextTestRunner(verbosity=3).run(suite)
+        # Read next line
+        line = fp.readline()
--- dataCollectortesting.py
+++ dataCollectortesting.py
@@ -1,59 +1,71 @@
 import json, unittest, main
 
+#Temp values
 test_values = '{ "location": 1, "depth" : 0, "temperature" : 15, "pH" : 1, "turbidity": 300}'
 collected_values = json.loads(test_values)
 
 class AcceptedRangeTest(unittest.TestCase):
+    # Testing upper bound of the temperature sensor's range
     def test_Temperature_Range_High(self):
         self.assertTrue(collected_values["temperature"] < 150)
 
+    # Testing lower bound of the temperature sensor's range
     def test_Temperature_Range_Low(self):
         self.assertTrue(collected_values["temperature"] > -50)
 
+    # Testing upper bound of the pH sensor's range
     def test_pH_Range_High(self):
-         self.assertTrue(collected_values["pH"] <=14, msg= '{0}')
+         self.assertTrue(collected_values["pH"] <=14)
 
+    # Testing lower bound of the pH sensor's range
     def test_pH_Range_Low(self):
         self.assertTrue(collected_values["pH"] >= 0)
 
+    # Testing upper bound of the turbidity sensor's range
     def test_Turbidity_Range_High(self):
         self.assertTrue(collected_values["turbidity"] <= 3000)
 
+    # Testing lower bound of the turbidity sensor's range
     def test_Turbidity_Range_Low(self):
         self.assertTrue(collected_values["turbidity"] >= 0)
 
-'''
-Is there a way to comment on data if the temperature range is out of bounds 
-to note that this data is collected outside of the accepted range of '''
+
 class TemperatureRangeTest(unittest.TestCase):
+    # Testing upper bound of turbidity sensor temperature accuracy
     def test_Temperature_High(self):
         self.assertTrue(collected_values["temperature"] <= 125)
 
+    # Testing lower bound of temperature sensor temperature accuracy
     def test_Temperature_Low(self):
         self.assertTrue(collected_values["temperature"] >= -50)
 
+    # Testing lower bound of pH sensor temperature accuracy
     def test_pH_Temperature_High(self):
         self.assertTrue(collected_values["temperature"] <= 50)
 
+    # Testing lower bound of pH sensor temperature accuracy
     def test_pH_Temperature_Low(self):
         self.assertTrue(collected_values["temperature"] >= -10)
 
+    # Testing upper bound of turbidity sensor temperature accuracy
     def test_Turbidity_Temperature_High(self):
         self.assertTrue(collected_values["temperature"] <= 90)
 
+    # Testing lower bound of turbidity sensor temperature accuracy
     def test_Turbidity_Temperature_Low(self):
         self.assertTrue(collected_values["temperature"] >= 5)
 
 class TestOther(unittest.TestCase):
+    # Gets all location values from database and checks if in range
     def test_location(self):
         location_array = main.query("SELECT * FROM locations")
         self.assertTrue(1 <= collected_values["location"] <= len(location_array))
 
-    # can we make this testing dynamic?
+    # Checks if depth is a specific value(s)
     def test_depth(self):
         depths = [0, 0.25, 0.50, 0.75]
         self.assertTrue(collected_values["depth"] in depths)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main();
